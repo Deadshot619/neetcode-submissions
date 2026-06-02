d3e3c798918f03877e2e10077d82af2b42262639
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-06-02 18:34:32 +0530

    Add: min-cost-climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-5.kt
@@ -0,0 +1,44 @@
+class Solution {
+    fun minCostClimbingStairs(cost: IntArray): Int {
+        // return backtracking(cost)
+        // return topDown(cost)
+        return bottomUp(cost)
+    }
+
+    fun backtracking(cost: IntArray): Int {
+        
+        fun dfs(i: Int): Int {
+            if(i >= cost.size) return 0
+
+            return cost[i] + minOf(dfs(i + 1), dfs(i + 2))
+        }
+
+        return minOf(dfs(0), dfs(1))
+    }
+
+    fun topDown(cost: IntArray): Int {
+        val cache = mutableMapOf<Int, Int>()
+
+        fun dfs(i: Int): Int {
+            if(i >= cost.size) return 0
+
+            if(i in cache) return cache[i]!!
+
+            cache[i] = cost[i] + minOf(dfs(i + 1), dfs(i + 2))
+
+            return cache[i]!!
+        }
+
+        return minOf(dfs(0), dfs(1))
+    }
+
+    fun bottomUp(cost: IntArray): Int {
+        val dp = IntArray(cost.size + 1)
+
+        for(i in 2 until dp.size) {
+            dp[i] = minOf(cost[i - 1] + dp[i - 1], cost[i - 2] + dp[i - 2])
+        }
+
+        return dp.last()
+    }
+}
