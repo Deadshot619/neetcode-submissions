98a909b9cc5fc5eb3fc338e81611d05b1811d595
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-06-03 18:48:14 +0530

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.kt
@@ -0,0 +1,77 @@
+class Solution {
+    fun numDecodings(s: String): Int {
+        // return backtracking(s)
+        // return topDown(s)
+        return bottomUp(s)
+    }
+
+    fun backtracking(s: String): Int {
+    
+        fun dfs(i: Int): Int {
+            if(i >= s.length) return 1
+
+            if(s[i] == '0') return 0
+
+            var total = dfs(i + 1)
+
+            if(i + 1 < s.length){
+                if(s[i] == '1' || (s[i] == '2' && s[i+1] < '7')){
+                    total += dfs(i+2)
+                }
+            }
+
+            return total
+        }
+
+        return dfs(0)
+    }
+
+    fun topDown(s: String): Int {
+        val dp = mutableMapOf<Int, Int>()
+
+        fun dfs(i: Int): Int {
+            if(i >= s.length) return 1
+
+            if(s[i] == '0') return 0
+
+            if(i in dp) return dp[i]!!
+
+            var total = dfs(i + 1)
+
+            if(i + 1 < s.length){
+                if(s[i] == '1' || (s[i] == '2' && s[i+1] < '7')){
+                    total += dfs(i+2)
+                }
+            }
+
+            dp[i] = total
+            return dp[i]!!
+        }
+
+        return dfs(0)
+    }
+
+    fun bottomUp(s: String): Int {
+        val dp = mutableMapOf<Int, Int>()
+        dp[s.length] = 1
+
+        for(i in s.length - 1 downTo 0) {
+            if(s[i] == '0') {
+                dp[i] = 0
+                continue
+            }
+
+            var total = dp[i + 1]!!
+
+            if(i + 1 < s.length){
+                if(s[i] == '1' || (s[i] == '2' && s[i+1] < '7')){
+                    total = total + dp[i + 2]!!
+                }
+            }
+
+            dp[i] = total
+        }
+
+        return dp[0]!!
+    }
+}
