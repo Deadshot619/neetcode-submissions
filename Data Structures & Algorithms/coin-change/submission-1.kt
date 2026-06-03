31ba358ab496ae6d6d04878fc0a310b0bd266d5e
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-06-03 20:31:13 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.kt
@@ -0,0 +1,57 @@
+class Solution {
+    fun coinChange(coins: IntArray, amount: Int): Int {
+        // return backtracking(coins, amount)
+        return topDown(coins, amount)
+    }
+
+    fun backtracking(coins: IntArray, amount: Int): Int {
+        
+        val res = mutableListOf<Int>()
+        var minSize = Integer.MAX_VALUE
+
+        fun dfs(i: Int, total: Int) {
+            if(total == amount) {
+                minSize = minOf(res.size, minSize)
+            }
+
+            if(i >= coins.size || total > amount) return
+
+            res.add(i)
+            dfs(i, total + coins[i])
+
+            res.removeLast()
+            dfs(i + 1, total)
+        }
+
+        dfs(0, 0)
+        return if(minSize == Integer.MAX_VALUE) -1 else minSize
+    }
+
+    fun topDown(coins: IntArray, amount: Int): Int {
+        
+        val memo = HashMap<Pair<Int, Int>, Int>()
+
+        fun dfs(i: Int, total: Int): Int {
+            if (total == amount) return 0
+            if (i >= coins.size || total > amount) return Int.MAX_VALUE
+
+            val key = Pair(i, total)
+            if (memo.containsKey(key)) return memo[key]!!
+
+            // Choice 1: take coin[i]
+            val take = dfs(i, total + coins[i])
+            val takeResult = if (take == Int.MAX_VALUE) Int.MAX_VALUE else 1 + take
+
+            // Choice 2: skip coin[i]
+            val skip = dfs(i + 1, total)
+
+            val result = minOf(takeResult, skip)
+            memo[key] = result
+            return result
+        }
+
+        val ans = dfs(0, 0)
+        return if (ans == Int.MAX_VALUE) -1 else ans
+
+    }
+}
