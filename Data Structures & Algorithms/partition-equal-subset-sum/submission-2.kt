6c05509694976cdbe45b05e6b9d2d78be5919961
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-06-06 18:21:32 +0530

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun canPartition(nums: IntArray): Boolean {
+        return bruteForce(nums)
+    }
+
+    fun bruteForce(nums: IntArray): Boolean {
+        val sum = nums.sum()
+
+        if(sum%2 != 0) return false
+
+        var target = sum/2
+        val cache = HashMap<Pair<Int, Int>, Boolean>()
+        fun dfs(i: Int, sum: Int): Boolean {
+            if(sum == target)
+                return true
+            
+            if(sum > target || i >= nums.size)
+                return false
+
+            val p = Pair(i, sum)
+            if(p in cache) return cache[p]!!
+
+            cache[p] = dfs(i + 1, nums[i] + sum) || dfs(i + 1, sum) 
+
+            return cache[p]!!
+        }
+
+        return dfs(0, 0)
+    }
+}
