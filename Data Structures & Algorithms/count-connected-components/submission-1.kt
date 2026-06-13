e7597f74e8a6de378680c22c627b7bbd31d62c51
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-06-13 18:04:49 +0530

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.kt
@@ -0,0 +1,39 @@
+class Solution {
+    fun countComponents(n: Int, edges: Array<IntArray>): Int {
+        return usingDfs(n, edges)
+    }
+
+    fun usingDfs(n: Int, edges: Array<IntArray>): Int {
+        val adj = HashMap<Int, MutableList<Int>>()
+        val visited = BooleanArray(n)
+
+        for(i in 0 until n) {
+            adj[i] = mutableListOf<Int>()
+        }
+
+        for((e1, e2) in edges) {
+            adj[e1]?.add(e2)
+            adj[e2]?.add(e1)
+        }
+
+        fun dfs(n: Int) {
+            if(visited[n]) return
+
+            visited[n] = true
+
+            for(e in adj[n] ?: listOf()) {
+                dfs(e)
+            }
+        }
+
+        var res = 0
+        for(e in 0 until n) {
+            if(!visited[e]) {
+                dfs(e)
+                res++
+            }
+        }
+
+        return res
+    }
+}
