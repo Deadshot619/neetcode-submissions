cef6a3ca52898be929b1a7df5f67c4f2b3f4394e
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-06-14 17:40:54 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun uniquePaths(m: Int, n: Int): Int {
+        return bottomUp(m,n)
+    }
+
+    fun bottomUp(m: Int, n: Int): Int {
+        val dp = Array(m) {
+            IntArray(n) { 1 }
+        }
+
+        for(i in m - 2 downTo 0) {
+            for(j in n - 2 downTo 0) {
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
+            }
+        }
+
+        return dp[0][0]
+    }
+}
