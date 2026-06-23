6d48c3ac24e7018c87d705fc35371438a2c6134f
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-06-23 21:35:08 +0530

    Add: jump-game-ii - submission-5

---
+++ Data Structures & Algorithms/jump-game-ii/submission-5.kt
@@ -0,0 +1,46 @@
+class Solution {
+    fun jump(nums: IntArray): Int {
+        // return backtracking(nums)
+        return topDown(nums)
+    }
+
+    fun backtracking(nums: IntArray): Int {
+        
+        fun dfs(i: Int): Int {
+            if(i >= nums.size - 1) return 0
+            if(nums[i] == 0) return 1000000
+
+            var res = Integer.MAX_VALUE
+
+            for(j in 1..nums[i]) {
+                res = min(res, 1 + dfs(i + j))
+            }
+
+            return res
+        }
+
+        return dfs(0)
+    }
+
+    fun topDown(nums: IntArray): Int {
+        val dp = HashMap<Int, Int>()
+        fun dfs(i: Int): Int {
+            if(i >= nums.size - 1) return 0
+            if(nums[i] == 0) return 1000000
+
+            if(i in dp) return dp[i]!!
+
+            var res = Integer.MAX_VALUE
+
+            for(n in 1..nums[i]) {
+                res = min(res, 1 + dfs(i + n))
+            }
+
+            dp[i] = res
+
+            return res
+        }
+
+        return dfs(0)
+    }
+}
