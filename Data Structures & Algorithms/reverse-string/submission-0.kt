53b6e9fcceebfa5d33d7afd611cec65c09fce556
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-06-28 17:40:21 +0530

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun reverseString(s: CharArray) {
+        twoPointer(s)
+    }
+
+    fun twoPointer(s: CharArray) {
+        var l = 0
+        var r = s.size - 1
+
+        while(l < r) {
+            val temp = s[l]
+            s[l] = s[r]
+            s[r] = temp
+
+            l++
+            r--
+        }
+    }
+}
