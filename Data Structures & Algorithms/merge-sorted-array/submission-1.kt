e45b28e724eac49ed6ea9bee9cf8796a6e1d9faa
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-06-28 19:19:26 +0530

    Add: merge-sorted-array - submission-1

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-1.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
+        var l1 = m - 1
+        var l2 = n - 1
+        var last = m + n - 1
+
+        while(l1 >= 0 && l2 >= 0) {
+            if(nums1[l1] <= nums2[l2]) {
+                nums1[last] = nums2[l2]
+                l2--
+            } else {
+                nums1[last] = nums1[l1]
+                l1--
+            }
+            last--
+        }
+
+        // Add leftovers from nums2
+        while(l2 >= 0) {
+            nums1[last] = nums2[l2]
+            l2--
+            last--
+        }
+    }
+}
