0725bcdb02a416cfdd22f7fd5bc737dae82b97e4
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-06-30 18:51:48 +0530

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun subarraySum(nums: IntArray, k: Int): Int {
+        return bruteForce(nums, k)
+    }
+
+    fun bruteForce(nums: IntArray, k: Int): Int {
+        var count = 0
+
+        for(i in 0 until nums.size) {
+            var sum = 0
+            for(j in i until nums.size) {
+                sum += nums[j]
+                if(sum == k) count++
+            }
+        }
+
+        return count
+    }
+}
