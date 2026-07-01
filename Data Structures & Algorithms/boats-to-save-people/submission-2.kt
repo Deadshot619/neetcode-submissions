2033dd42dd46228d71e42d9ef5f1570a5b66414e
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-07-02 01:59:09 +0530

    Add: boats-to-save-people - submission-2

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-2.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun numRescueBoats(people: IntArray, limit: Int): Int {
+        return optimal(people, limit)
+    }
+
+    fun optimal(people: IntArray, limit: Int): Int {
+        people.sort()
+
+        var l = 0
+        var r = people.size - 1
+
+        var count = 0
+
+        while(l <= r) {
+            val sum = people[l] + people[r]
+
+            if(sum > limit) { 
+                r--
+            } else {
+                l++
+                r--
+            }
+            count++
+        }
+
+        return count
+    }
+}
