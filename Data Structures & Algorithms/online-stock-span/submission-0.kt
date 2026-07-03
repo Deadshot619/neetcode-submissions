036d81fbd4867ba7535723a793ef313497c40320
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-07-03 19:41:32 +0530

    Add: online-stock-span - submission-0

---
+++ Data Structures & Algorithms/online-stock-span/submission-0.kt
@@ -0,0 +1,26 @@
+class StockSpanner() {
+
+    val stack = ArrayDeque<Pair<Int, Int>>() // num, index
+    var c = 0
+
+    fun next(price: Int): Int {
+        var poppedItem: Pair<Int, Int>? = null
+
+        while(stack.isNotEmpty() && stack.last().first <= price) {
+            poppedItem = stack.removeLast()
+        }
+
+        val item = Pair(price, poppedItem?.second ?: c)
+        stack.addLast(item)
+        val res = if(poppedItem == null) 1 else c - poppedItem!!.second + 1
+        c++
+        println("$item, $poppedItem")
+        return res
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * val obj = StockSpanner()
+ * val param_1 = obj.next(price)
+ */
