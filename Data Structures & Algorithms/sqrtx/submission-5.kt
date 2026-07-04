bcff7da03b0690b2e4cd374f2c1f66b2e2e170c4
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-07-04 19:18:37 +0530

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun mySqrt(x: Int): Int {
+        return binarySearch(x)
+    }
+
+    fun binarySearch(x: Int): Int {
+        if(x < 2) return x
+
+        var l = 0
+        var r = x/2
+        var res = 0
+
+        while(l <= r) {
+            val mid = l + (r - l)/2
+            val sq = mid.toLong() * mid
+
+            if(sq > x) {
+                r = mid - 1
+            } else if(sq < x) {
+                l = mid + 1
+                res = mid
+            } else
+                return mid
+        }
+
+        return res
+    }
+}
