58f3acb723699f2a6d57fb6b85b7d98251e04463
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-07-07 20:44:49 +0530

    Add: reverse-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-2.kt
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun reverseList(head: ListNode?): ListNode? {
+        // return twoPointer(head)
+        // return recursive(head)
+        return recursive2(head)
+    }
+
+    fun twoPointer(head: ListNode?): ListNode? {    // T: O(n), S: O(1)
+        var prev: ListNode? = null
+        var cur = head
+
+        while(cur != null) {
+            val temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+        }
+
+        return prev
+    }
+
+    fun recursive(head: ListNode?): ListNode? {    // T: O(n), S: O(n)
+        if(head == null)
+            return null
+
+        var newHead = head
+        if(head?.next != null) {
+            newHead = recursive(head.next)
+            head?.next?.next = head
+        }
+        head.next = null
+        
+        return newHead
+    }
+
+    fun recursive2(head: ListNode?): ListNode? {    // T: O(n), S: O(n)
+        if(head == null || head?.next == null)
+            return head
+
+        val newHead = recursive2(head?.next)
+        head?.next?.next = head
+        head?.next = null
+
+        return newHead
+    }
+}
