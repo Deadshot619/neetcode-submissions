3c913b46f4a1d83c45a3950cfcc0c0c4044e552b
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-07-11 15:30:51 +0530

    Add: binary-tree-inorder-traversal - submission-2

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-2.kt
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        // return inorder(root)
+        return iterative(root)
+    }
+
+    fun inorder(root: TreeNode?): List<Int> {
+        var res = mutableListOf<Int>()
+
+        fun dfs(node: TreeNode?) {
+            if(node == null) return
+
+            dfs(node?.left)
+            res.add(node.`val`)
+            dfs(node?.right)
+        }
+
+        dfs(root)
+        return res
+    }
+
+    fun iterative(root: TreeNode?): List<Int> {
+        val res = mutableListOf<Int>()
+        val stack = mutableListOf<TreeNode>()
+        var cur: TreeNode? = root
+
+        while(cur != null || stack.isNotEmpty()) {
+            while(cur != null) {
+                stack.add(cur)
+                cur = cur?.left
+            }
+            cur = stack.removeLast()
+            res.add(cur.`val`)
+            cur = cur?.right
+        }
+
+        return res
+    }
+}
