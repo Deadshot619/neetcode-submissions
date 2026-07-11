df03cbe6fbc24bebe7dd3927390c2f79807786a6
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-07-11 15:36:29 +0530

    Add: binary-tree-preorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-1.kt
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun preorderTraversal(root: TreeNode?): List<Int> {
+        // return preorder(root)
+        return iterative(root)
+    }
+
+    fun preorder(root: TreeNode?): List<Int> {
+        var res = mutableListOf<Int>()
+
+        fun dfs(node: TreeNode?) {
+            if(node == null) return
+
+            res.add(node.`val`)
+            dfs(node?.left)
+            dfs(node?.right)
+        }
+
+        dfs(root)
+        return res
+    }
+
+    fun iterative(root: TreeNode?): List<Int> {
+        val res = mutableListOf<Int>()
+        val stack = mutableListOf<TreeNode>()
+        var cur: TreeNode? = root
+
+        while(cur != null || stack.isNotEmpty()) {
+            while(cur != null) {
+                res.add(cur.`val`)
+                stack.add(cur)
+                cur = cur?.left
+            }
+            cur = stack.removeLast()
+            cur = cur?.right
+        }
+
+        return res
+    }
+}
