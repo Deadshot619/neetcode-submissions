664fca1c21a65c0b7634da11686e6863d0d9641d
Pankaj Makhija <pankaj.makhija26@gmail.com>
2026-08-01 21:48:05 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun getConcatenation(nums: IntArray): IntArray {
+        return singlePass(nums)
+    }
+
+    fun singlePass(nums: IntArray): IntArray {        
+        val n = nums.size
+        val res = IntArray(n + n)
+        for(i in 0 until n) {
+            res[i] = nums[i]
+            res[i + n] = nums[i]
+        }
+
+        return res
+    }
+}
